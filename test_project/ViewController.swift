e8cd6211f64eb568de7941c359cffe806970fdda
Lena Sokolova <sokolovalena1999@mail.ru>
2021-08-09 16:55:48 +0300

    task

---
+++ test_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_project
+//
+//  Created by Mac Mini 2018 on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
